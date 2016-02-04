b0eaa281b01fc5ec6d9cb677deea506d851ad534
EDFward <edfward@gmail.com>
2016-02-03 20:56:55 -0800

    Index training texts

---
+++ src/main/java/com/edfward/homedepot/Indexing.java
@@ -0,0 +1,116 @@
+package com.edfward.homedepot;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.en.EnglishAnalyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.file.FileSystems;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiFunction;
+
+class Product {
+  String title = "";
+  String description = "";
+  // TODO: Init attributes. Ignore for now.
+  Map<String, String> attributes;
+
+  @Override
+  public String toString() {
+    final int TITLE_LEN = 30, DESC_LEN = 50;
+    String slicedTitle = title.length() < TITLE_LEN ? title : (title.substring(0, TITLE_LEN) + "...");
+    String slicedDescription = description.length() < DESC_LEN ? description : (description.substring(0, DESC_LEN) + "...");
+    return "title: '" + slicedTitle + "', desc: '" + slicedDescription + "'";
+  }
+}
+
+public class Indexing {
+
+  private static final String PRODUCT_ID = "product_uid";
+
+  public static void main(String[] args) throws IOException {
+    Indexing indexing = new Indexing();
+    indexing.index();
+  }
+
+  private void parse(
+      String filePath,
+      Map<Long, Product> products,
+      // Pass a lambda to modify the product.
+      BiFunction<Product, CSVRecord, Void> productModifier) throws IOException {
+    Reader reader = new InputStreamReader(getClass().getResourceAsStream(filePath));
+    CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT.withHeader());
+    for (CSVRecord record : csvParser) {
+      long id = Long.parseLong(record.get(PRODUCT_ID));
+      Product product = products.get(id);
+      if (product == null) {
+        product = new Product();
+        products.put(id, product);
+      }
+      productModifier.apply(product, record);
+    }
+  }
+
+  // Parse `train.csv` and `test.csv` for product titles.
+  public void parseTitle(Map<Long, Product> products) throws IOException {
+    BiFunction<Product, CSVRecord, Void> modifier = (product, record) -> {
+      product.title = record.get("product_title");
+      return null;
+    };
+
+    parse("/train.csv", products, modifier);
+    parse("/test.csv", products, modifier);
+  }
+
+  // Parse `product_descriptions.csv` for product descriptions.
+  public void parseDescription(Map<Long, Product> products) throws IOException {
+    BiFunction<Product, CSVRecord, Void> modifier = (product, record) -> {
+      product.description = record.get("product_description");
+      return null;
+    };
+    parse("/product_descriptions.csv", products, modifier);
+  }
+
+  public void index() throws IOException {
+    Map<Long, Product> products = new HashMap<>();
+    // 'Decorate' the product hashmap.
+    parseTitle(products);
+    parseDescription(products);
+    // TODO: Parse attributes.
+
+    // Init Lucene stuff.
+    Analyzer analyzer = new StandardAnalyzer();
+    Directory directory = FSDirectory.open(FileSystems.getDefault().getPath("index"));
+    IndexWriterConfig config = new IndexWriterConfig(analyzer);
+    IndexWriter writer = new IndexWriter(directory, config);
+
+    // Write index.
+    for (Map.Entry<Long, Product> entry : products.entrySet()) {
+      Long id = entry.getKey();
+      Product product = entry.getValue();
+      Document doc = new Document();
+      // Do not analyze ID.
+      doc.add(new StringField("id", id.toString(), Field.Store.YES));
+      // Analyze title and description.
+      doc.add(new TextField("title", product.title, Field.Store.NO));
+      doc.add(new TextField("description", product.description, Field.Store.NO));
+
+      writer.addDocument(doc);
+    }
+    writer.close();
+  }
+}
